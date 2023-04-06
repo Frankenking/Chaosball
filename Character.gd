db4cce37f8f0599011f87105b857886047477bdd
Frankenking <99056962+Frankenking@users.noreply.github.com>
2023-04-06 09:46:30 -0700

    yh

---
+++ Character.gd
@@ -0,0 +1,30 @@
+extends RigidBody3D
+
+var velocity = Vector3()
+func input():
+	if Input.is_action_pressed("forward"):
+		velocity.z = 10
+	else:
+		velocity.z = 0
+		
+	if Input.is_action_pressed("back"):
+		velocity.z = 10
+	else:
+		velocity.z = 0
+		
+	if Input.is_action_pressed("left"):
+		velocity.x = -10
+	else:
+		velocity.x = 0
+		
+	if Input.is_action_pressed("right"):
+		velocity.x = 10
+	else:
+		velocity.x = 0
+	
+	
+	return velocity
+		
+func _process(delta):
+	velocity = input()
+	$".".linear_velocity = velocity
--- main.gd
+++ main.gd
@@ -4,6 +4,7 @@ extends Node3D
 var rng = RandomNumberGenerator.new()
 var randnum1 = 0
 var randnum2 = 0
+var vector = Vector3()
 
 func _ballposcheck() -> void:
 	
@@ -29,23 +30,22 @@ func _ballposcheck() -> void:
 				
 func _ballvel() -> void:
 	
-	randnum1 = rng.randf_range(-30.0, 30.0)
-	randnum2 = rng.randf_range(-30.0, 30.0)
+	randnum1 = rng.randf_range(-50.0, 50.0)
+	randnum2 = rng.randf_range(-50.0, 50.0)
 	$Balls/Chaos_Ball1.linear_velocity = Vector3(randnum1, 0, randnum2)
 	
-	randnum1 = rng.randf_range(-30.0, 30.0)
-	randnum2 = rng.randf_range(-30.0, 30.0)
+	randnum1 = rng.randf_range(-50.0, 50.0)
+	randnum2 = rng.randf_range(-50.0, 50.0)
 	$Balls/Chaos_Ball2.linear_velocity = Vector3(randnum1, 0, randnum2)
 	
-	randnum1 = rng.randf_range(-30.0, 30.0)
-	randnum2 = rng.randf_range(-30.0, 30.0)
+	randnum1 = rng.randf_range(-50.0, 50.0)
+	randnum2 = rng.randf_range(-50.0, 50.0)
 	$Balls/Chaos_Ball3.linear_velocity = Vector3(randnum1, 0, randnum2)
 	
-	randnum1 = rng.randf_range(-30.0, 30.0)
-	randnum2 = rng.randf_range(-30.0, 30.0)
+	randnum1 = rng.randf_range(-50.0, 50.0)
+	randnum2 = rng.randf_range(-50.0, 50.0)
 	$Balls/Chaos_Ball4.linear_velocity = Vector3(randnum1, 0, randnum2)
-	
-	
+		
 func _ready():
 	
 	$Balls/Chaos_Ball1.linear_velocity = Vector3(10, 0, 0)
@@ -59,3 +59,31 @@ func _process(_delta):
 		$ContactTimer.stop()
 		_ballvel()
 		$ContactTimer.start()
+
+func _on_red_rg_body_entered(body):
+	if body == $GoalBalls/RedBall:
+		$GoalBalls/RedBall.hide()
+		$GoalBalls/GreenBall.translate(Vector3(-50,0,0))
+		$GoalBalls/GreenBall.show()
+		$GoalBalls/GreenBall.freeze = false
+
+func _on_green_rg_body_entered(body):
+	if body == $GoalBalls/GreenBall:
+		$GoalBalls/GreenBall.hide()
+		$GoalBalls/BlueBall.translate(Vector3(-50,0,0))
+		$GoalBalls/BlueBall.show()
+		$GoalBalls/BlueBall.freeze = false
+
+func _on_blue_rg_body_entered(body):
+	if body == $GoalBalls/BlueBall:
+		$GoalBalls/BlueBall.hide()
+		$GoalBalls/YellowBall.translate(Vector3(-50,0,0))
+		$GoalBalls/YellowBall.show()
+		$GoalBalls/YellowBall.freeze = false
+
+		
+
+
+func _on_yellow_rg_body_entered(body):
+	if body == $GoalBalls/YellowBall:
+		$GoalBalls/YellowBall.hide()
