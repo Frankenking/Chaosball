1ce7271c33d732767bc5b9aacb04578d11c65211
Frankenking <99056962+Frankenking@users.noreply.github.com>
2023-04-10 20:20:37 -0700

    .7

--- main.gd
+++ main.gd
@@ -16,27 +16,52 @@ var vector = Vector3()
 @onready var Ball3 := $Balls/Chaos_Ball3
 @onready var Ball4 := $Balls/Chaos_Ball4
 
+var ballRed = true
+var ballYellow = true
+var ballBlue = true
+var ballGreen = true
+
 var points = 0
 
 func _ballposcheck() -> void:
 	
-				
 	for index in Ball1.position:
-			if abs(index) > 25:
-				Ball1.position = Vector3(0, 1, 0)
-				
+		if abs(index) > 25:
+			Ball1.position = Vector3(0, 1, 0)
+	
 	for index in Ball2.position:
-			if abs(index) > 25:
-				Ball2.position = Vector3(0, 1, 0)
-				
+		if abs(index) > 25:
+			Ball2.position = Vector3(0, 1, 0)
+	
 	for index in Ball3.position:
-			if abs(index) > 25:
-				Ball3.position = Vector3(0, 1, 0)
-				
+		if abs(index) > 25:
+			Ball3.position = Vector3(0, 1, 0)
+	
 	for index in Ball4.position:
+		if abs(index) > 25:
+			Ball4.position = Vector3(0, 1, 0)
+
+func _goalposcheck():
+	if ballRed == true:
+		for index in Redball.position:
 			if abs(index) > 25:
-				Ball4.position = Vector3(0, 1, 0)
-				
+				$GoalBalls/RedBall.position = Vector3(0, 1, 0)
+	
+	if ballYellow == true:
+		for index in Yellowball.position:
+			if abs(index) > 25:
+				$GoalBalls/YellowBall.position = Vector3(0, 1, 0)
+	
+	if ballBlue == true:
+		for index in Blueball.position:
+			if abs(index) > 25:
+				$GoalBalls/BlueBall.position = Vector3(0, 1, 0)
+	
+	if ballGreen == true:
+		for index in Greenball.position:
+			if abs(index) > 25:
+				$GoalBalls/GreenBall.position = Vector3(0, 1, 0)
+	
 func _ballvel() -> void:
 	
 	randnum1 = rng.randf_range(-50.0, 50.0)
@@ -54,7 +79,7 @@ func _ballvel() -> void:
 	randnum1 = rng.randf_range(-50.0, 50.0)
 	randnum2 = rng.randf_range(-50.0, 50.0)
 	Ball4.linear_velocity = Vector3(randnum1, 0, randnum2)
-		
+	
 func _ready():
 	
 	Ball1.linear_velocity = Vector3(10, 0, 0)
@@ -62,34 +87,37 @@ func _ready():
 	Ball3.linear_velocity = Vector3(0, 0, 10)
 	Ball4.linear_velocity = Vector3(0, 0, -10)
 
-
-
 func _process(_delta):
 	_ballposcheck()
+	_goalposcheck()
+	
 	if $ContactTimer.time_left <= 0.05:
 		$ContactTimer.stop()
 		_ballvel()
 		$ContactTimer.start()
 	if points == 4:
-		pass
+		get_tree().change_scene_to_file("res://win_screen.tscn")
 
 func _on_red_rg_body_entered(body):
 	if body == Redball:
 		Redball.queue_free()
 		points += 1
-		
+		ballRed = false
+	
 func _on_yellow_rg_body_entered(body):
 	if body == Yellowball:
 		Yellowball.queue_free()
 		points += 1
-
+		ballYellow = false
+	
 func _on_green_rg_body_entered(body):
 	if body == Greenball:
 		Greenball.queue_free()
 		points += 1
-		
+		ballGreen = false
+	
 func _on_blue_rg_body_entered(body):
 	if body == Blueball:
 		Blueball.queue_free()
 		points += 1
-		
+		ballBlue = false
