d645e9eb19d5cb43e5fed7b2ea9599b457bf710f
Frankenking <99056962+Frankenking@users.noreply.github.com>
2023-04-12 20:22:43 -0700

    .9

---
+++ play_presets.gd
@@ -0,0 +1,17 @@
+extends Node
+
+@export var debug = false
+@export var metallic = 0
+@export var rough = 0
+
+@export var player_texture = " "
+
+@export var ball_texture1 = " "
+@export var ball_texture2 = " "
+@export var ball_texture3 = " "
+@export var ball_texture4 = " "
+
+@export var redball_texture1 = " "
+@export var greenball_texture1 = " "
+@export var yellowball_texture1 = " "
+@export var blueball_texture1 = " "
--- title_screen.gd
+++ title_screen.gd
@@ -1,15 +1,23 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	pass
 func _process(delta):
 	pass
 
 
-func _on_button_pressed():
+func _on_play_pressed():
 	get_tree().change_scene_to_file("res://main.tscn")
+
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+
+func _on_settings_button_pressed():
+	$Settings.show()
+	$Main.hide()
+	
+func _on_back_pressed():
+	$Main.show()
+	$Settings.hide()
